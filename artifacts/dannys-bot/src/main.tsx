6457fa5f1c2b9e73387b5439947c3f2a9552d743
Danny <danny@dannysbot.local>
2026-05-01 21:42:46 +0000

    Remove exit warning dialog

--- artifacts/dannys-bot/src/main.tsx
+++ artifacts/dannys-bot/src/main.tsx
@@ -2,8 +2,4 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-window.onbeforeunload = function() {
-  return "Are you sure you want to exit? The bot will stop running.";
-};
-
 createRoot(document.getElementById("root")!).render(<App />);
